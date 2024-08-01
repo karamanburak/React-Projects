3da433117288ed4dec7dbba509158640c2ed4243
Burak Karaman <karaman.buraak@gmail.com>
2024-08-01 16:46:47 +0200

    Update App.jsx to change initial investment value, add input validation.

--- react-essential-practice/src/App.jsx
+++ react-essential-practice/src/App.jsx
@@ -5,26 +5,30 @@ import Results from "./components/Results"
 
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 1000,
+    initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10
   })
 
+  const inputIsValid = userInput.duration >= 1
+
   const handleChange = (inputIdentifier, newValue) => {
     setUserInput(prevUserInput => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue
+        [inputIdentifier]: +newValue
       }
     })
   }
+
 
   return (
     <>
       <Header />
       <UserInput handleChange={handleChange} userInput={userInput} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero...</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   )
 }
